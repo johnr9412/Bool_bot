9e1b43504ef5517cefd68fe951915a1afab0b578
John Robinson <jrrobinson@jrrobinson-Macbook-Server.local>
2022-03-18 20:28:36 -0400

    updated response

--- discord_permissions_lambda/discord_permissions_lambda.py
+++ discord_permissions_lambda/discord_permissions_lambda.py
@@ -47,15 +47,10 @@ def delete_permissions_records():
 
 
 def create_response(status_code, body=None):
+    response = { "statusCode": status_code }
     if body:
-        return {
-            "statusCode": status_code,
-            "body": body
-        }
-    else:
-        return {
-            "statusCode": status_code
-        }
+        response['body'] = body
+    return response
 
 
 def lambda_handler(event, context):
