7c41dcd773b8da258965b734be4ad70afc0fc5e8
John Robinson <johnr@Johns-MacBook-Pro.local>
2022-04-19 21:55:08 -0400

    updated to use dockerfile for album lambda

--- discord_bot/lib/api_manager.py
+++ discord_bot/lib/api_manager.py
@@ -1,6 +1,6 @@
 
 PERMISSIONS_API_URL = 'https://83odmhhpre.execute-api.us-east-2.amazonaws.com/default/discord_permissions_lambda'
-ALBUMS_API_URL = 'https://r54yt5jv97.execute-api.us-east-2.amazonaws.com/default/get_albums_lambda'
+ALBUMS_API_URL = 'https://69vs2jybyc.execute-api.us-east-2.amazonaws.com/default/get_albums_lambda'
 SCHEDULE_API_URL = 'https://w53anzeyii.execute-api.us-east-2.amazonaws.com/default/get_schedule_lambda'
 STEP_SCRAPE_URL = 'https://8dq3a5fp5a.execute-api.us-east-2.amazonaws.com/default/step_scraper_lambda'
 STEP_API_URL = 'https://9m28i4tgoc.execute-api.us-east-2.amazonaws.com/default/step_data_lambda'
